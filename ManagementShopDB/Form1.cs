b5f1d65b6533287031e05abb4e3fecc195e6c472
Богдан Валовый <bogdan10075@gmail.com>
2018-11-27 21:12:32 +0200

    add first script

--- ManagementShopDB/Form1.cs
+++ ManagementShopDB/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,61 @@ namespace ManagementShopDB
 {
     public partial class Form1 : Form
     {
+        SqlConnection sqlConnection;
+
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();          
+
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            string connection = @"Data Source=DESKTOP-CHJ1K79\SQLEXPRESS;Initial Catalog=ShopDB;Integrated Security=True";
+            this.sqlConnection = new SqlConnection(connection);
+
+            FillGridViewOfClients();
+            AutosizeGridView(dataGridView_Clients);
+        }
+
+        public static void AutosizeGridView(DataGridView dataGridView)
+        {
+            var height = 40;
+            foreach (DataGridViewRow dr in dataGridView.Rows)
+            {
+                height += dr.Height;
+            }
+            dataGridView.Height = height;
+        }
+
+        private void FillGridViewOfClients()
+        {
+            var select = "SELECT * FROM Shop.Client";
+            var dataAdapter = new SqlDataAdapter(select, this.sqlConnection);
+            var commandBuilder = new SqlCommandBuilder(dataAdapter);
+            var ds = new DataSet();
+            dataAdapter.Fill(ds);
+            dataGridView_Clients.ReadOnly = true;
+            dataGridView_Clients.DataSource = ds.Tables[0];
+        }
+
+        private void fillByToolStripButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.clientTableAdapter.FillBy(this.shopDBDataSet.Client);
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
         }
     }
 }
